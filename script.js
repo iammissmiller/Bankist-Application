18d431f0129b2a02cc37b6d654cc4f97bd934593
Praptee Miller <139339934+iammissmiller@users.noreply.github.com>
2025-10-18 23:49:04 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -115,21 +115,34 @@ console.log(accounts);
 
 let currentAccount;
 
-btnLogin.addEventListener('click' , function(e) {
-
-  // Prevent form from submitting
+btnLogin.addEventListener('click', function (e) {
   e.preventDefault();
 
-  currentAccount =  accounts.find(acc => acc.username === inputLoginUsername.value);
+  currentAccount = accounts.find(
+    acc => acc.username === inputLoginUsername.value
+  );
   console.log(currentAccount);
 
-  if (currentAccount.pin === Number(inputLoginPin.value)){
-
-    // Display UI and Message
-    labelWelcome.textContent = `Welcome back, ${currentAccount.owner.split(" ")[0]}`;
+  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+    // Display UI and message
+    labelWelcome.textContent = `Welcome back, ${
+      currentAccount.owner.split(' ')[0]
+    }`;
     containerApp.style.opacity = 100;
+
+    // Display movements
+    displayMovements(currentAccount.movements);
+
+    // Display balance
+    calcDisplayBalance(currentAccount.movements);
+
+    // Display summary
+    calcDisplaySummary(currentAccount);
+  } else {
+    alert('Invalid username or PIN');
   }
-})
+});
+
 
 
 const calcDisplayBalance = function(movements ){
@@ -138,19 +151,19 @@ const calcDisplayBalance = function(movements ){
   labelBalance.textContent =  `${balance} €`;
 };
 
-const calcDisplaySummary = function(movements){
+const calcDisplaySummary = function(acc){
 
-  const income = movements.filter(mov => mov > 0).reduce((acc,mov) => acc + mov , 0);
+  const income = acc.movements.filter(mov => mov > 0).reduce((acc,mov) => acc + mov , 0);
   labelSumIn.textContent = `${income} €`
 
-  const out = movements.filter(mov => mov < 0).reduce((acc,mov) => acc + mov , 0 );
+  const out = acc.movements.filter(mov => mov < 0).reduce((acc,mov) => acc + mov , 0 );
   labelSumOut.textContent = `${Math.abs(out)} €`
 
-  const interest = movements.filter(mov => mov > 0).map(deposit => (deposit * 1.2) / 100).filter((int , i , arr) => {return int >= 1}).reduce((acc , int) => acc + int , 0);
+  const interest = acc.movements.filter(mov => mov > 0).map(deposit => (deposit * acc.interestRate) / 100).filter((int , i , arr) => {return int >= 1}).reduce((acc , int) => acc + int , 0);
   labelSumInterest.textContent = `${interest} €`
 }
 
-calcDisplaySummary(account1.movements);
+calcDisplaySummary(account1);
 
 calcDisplayBalance(account1.movements);
 
